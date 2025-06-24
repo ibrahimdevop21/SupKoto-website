08125fe745e5ea110144f8210f4a3ddd3a706792
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2025-06-24 18:48:06 +0300

    chore: remove Vercel-specific configuration to allow auto-detection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
-      // Explicitly configure Tailwind
-      config: { path: './tailwind.config.js' },
+      applyBaseStyles: false
     }),
     react()
   ],
-  output: 'static',
-  adapter: vercel(),
   // Add built-in i18n routing
   i18n: {
     defaultLocale: 'en',
